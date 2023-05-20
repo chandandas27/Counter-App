8f0ccfc98dc204151bc82ff1539e99ebe851a429
Chandan Das <chandandasdivakar@gmail.com>
2023-05-21 00:38:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counter/Counter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Counter/>
+      
     </div>
-  );
+  )
 }
+
+//{property1: 'value1', property2: 'value2'}
+// function PlayingWithProps(properties) {
+  
+//   console.log(properties)
+//   console.log(properties.property1)
+//   console.log(properties.property2)
+  
+//   return (
+//     <div>Props</div>
+//   )
+// }
+
+// function PlayingWithProps( {property1, property2} ) {
+//   console.log(property1)
+//   console.log(property2)
+  
+//   return (
+//     <div>Props</div>
+//   )
+// }
+
 
 export default App;
---
+++ src/components/counter/CounterButton.jsx
@@ -0,0 +1,40 @@
+
+
+import {PropTypes} from 'prop-types'
+
+export default function CounterButton({by, incrementMethod, decrementMethod}) {    
+
+    
+
+    // function incrementCounterFunction() {
+      
+    //     incrementMethod(by)
+    // }
+
+    // function decrementCounterFunction() {
+     
+    //     decrementMethod(by)
+    // }
+
+    return (
+        <div className="Counter">
+            <div>
+                <button className="counterButton" 
+                        onClick={ () => incrementMethod(by)}
+                >+{by}</button>
+                <button className="counterButton" 
+                        onClick={ () => decrementMethod(by)}
+                >-{by}</button>
+ 
+            </div>
+        </div>
+    )
+}
+
+CounterButton.propTypes = {
+    by: PropTypes.number
+}
+
+CounterButton.defaultProps = {
+    by: 5
+}
